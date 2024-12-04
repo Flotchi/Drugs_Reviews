035379b97425ed771cdf67dbfdad892af757bde1
sp <sebastian.peroulakis@hotmail.com>
2024-12-04 18:13:31 +0100

    api finale

--- logic/LSTM.py
+++ logic/LSTM.py
@@ -5,7 +5,9 @@ import numpy as np
 from tensorflow.keras.preprocessing.text import text_to_word_sequence
 from gensim.models import Word2Vec
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+from tensorflow.keras import layers,Sequential
+from tensorflow.keras.layers import InputLayer,Dropout,BatchNormalization, Bidirectional,LSTM,Dense
+from tensorflow.keras.callbacks import EarlyStopping
 
 
 
@@ -44,7 +46,7 @@ def create_embeding():
     Xpad = pad_sequences(Xemb, dtype='float32', padding= 'post', maxlen=200)
     print('padding done')
 
-    return Xpad
+    return (Xpad, y)
 
 
 def save_padded():
@@ -55,5 +57,42 @@ def save_padded():
     return None
 
 
+def init_model():
+    model = Sequential()
+    model.add(InputLayer((200,60)))
+    model.add(layers.Masking(mask_value=0.))
+    model.add(Bidirectional(LSTM(64, activation='tanh', return_sequences=False)))
+    model.add(Dropout(0.3))
+    model.add(Dense(32, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
+
+
+def train_model(X,y):
+    model = init_model()
+    es = EarlyStopping(patience=5, restore_best_weights=True)
+
+    model.fit(X, y,
+          batch_size=8,
+          epochs=50,
+          validation_split=0.3,
+          callbacks=[es]
+         )
+
+    path = os.path.dirname(os.path.dirname(__file__))
+    joblib.dump(model, os.path.join(path,'models', 'LSTM.pkl'))
+    return model
+
+
+
+
 if __name__ == '__main__':
-    save_padded()
+    # save_padded()
+    path = os.path.dirname(os.path.dirname(__file__))
+    X, y = joblib.load(os.path.join(path,'models', 'Xpad.pkl'))
+    train_model(X,y)
